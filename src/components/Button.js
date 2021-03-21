0f9f904b241480a081fe6eba65a1181a2cb4e49f
Alec Venzor <alecvenzordev@gmail.com>
2021-03-21 16:42:18 -0700

    fix 2

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,13 +9,20 @@ const Wrapper = styled.div`
 const CustomButton = styled.button`
   padding-top: 0.5em;
   padding-bottom: 0.5em;
-  padding-left: 2em;
-  padding-right: 2em;
+  padding-left: 2.5em;
+  padding-right: 2.5em;
   background-color: #8c6034;
   border: none;
   border-radius: 12px;
   color: white;
   font-size: 1.5em;
+  @media screen and (min-width: 768px) {
+    font-size: 1.8em;
+    padding-left: 3em;
+    padding-right: 3em;
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+  }
 `;
 function Button({ tag, route }) {
   return (
